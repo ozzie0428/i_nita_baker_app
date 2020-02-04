21ca7904be267d9af917c18d90579d021081ad87
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-02-04 16:12:18 -0500

    create project folder

---
+++ I_nita_baker_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class INitaBakerAppConfig(AppConfig):
+    name = 'I_nita_baker_app'
---
+++ I_nita_baker_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for I_nita_baker_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I_nita_baker_project.settings')
+
+application = get_asgi_application()
