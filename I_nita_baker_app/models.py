8ff01404f9847bf204333d576329c82a6d00fa18
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-02-04 18:08:47 -0500

    add models

--- I_nita_baker_app/models.py
+++ I_nita_baker_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Recipes(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients  = models.CharField(max_length=100)
+    instructions = models.TextField(default='', blank=True)
+    picture_url = models.TextField(default='', blank=true)
+    time = models.CharField(max_length=30)
+
+    def __str__(self)
+        return self.name
+
+
+class Reviews(models.Model):
+    name = models.CharField(max_length=100)
+    tastiness = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=100)
+    recipe = models.ForeignKey(Recipes, on_delete=models.CASCADE, related_name='reviews')
+
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=100)
+    picture_url = models.TextField(default='', blank=True)
+    price = models.CharField(max_length=100)
