8b6e2f2224a66820f3a1213b0ddfd9127bd4a844
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-02-07 13:15:20 -0500

    create forms for shoppinglist

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -14,7 +14,7 @@ export default class ShoppingList extends Component {
       }
     
       updateShoppingListPage = () => {
-        axios.get("/api/v1/shoppingList/").then(res => {
+        axios.get("/api/v1/shoppinglist/").then(res => {
           console.log("/api/v1/shoppingList/", res.data);
           this.setState({ shoppingListList: res.data });
         });
@@ -28,17 +28,56 @@ export default class ShoppingList extends Component {
           picture_url: this.state.picture_url
         };
         try {
-          const response = await axios.post("api/v1/shoppingList/", newShoppingList);
+          const response = await axios.post("api/v1/shoppinglist/", newShoppingList);
           console.log("TCL: ShoppingList -> createShoppingList -> response", response);
         } catch (error) {
           console.log("TCL: ShoppingList -> createShoppingList -> error", error.message);
         }
       };
 
+      handleChange = event => {
+        const inputValue = event.target.value;
+        this.setState({ [event.target.name]: inputValue });
+      };
+
     render() {
-        return (
-            <div>
+        const shoppingListList = 
+        this.state.shoppingListList &&
+        this.state.shoppingListList.map((shoppingList, i) => {
+            return(
+                <div>
+
+                    {shoppingList.name}
+                    {` $${shoppingList.price}`}
+                </div>
+                )
+                
+            })
+            return (
+                <div>
+                   <div>
+                    <input 
+                    type="string"
+                    name="newShoppingListName"
+                    placeholder="Name of Ingredient"
+                    required="required"
+                    onChange={this.handleChange}
+                    value={this.state.name}
+                    />
+
+                    <input 
+                    type="string"
+                    name="price"
+                    placeholder="Price of Ingredient"
+                    required="required"
+                    onChange={this.handleChange}
+                    value={this.state.price}
+                    />
+                    <button onClick={this.createShoppingList}> Add to Shopping List</button>
+                    
+                    </div> 
                 Hello FROM SHOPPING LIST
+                {shoppingListList}
             </div>
         )
     }
