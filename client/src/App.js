b3689fa9a493e826ef21c80c37d151dd06779a55
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-02-07 14:27:34 -0500

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -14,14 +14,25 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <div className="nav-bar">
+          <a href="/recipes">
+            <h3>Recipes</h3>
+          </a>
+          <a href="/">
+            <h3>Home</h3>
+          </a>
+          <a href="/shoppinglist">
+            <h3>Shopping List</h3>
+          </a>
+        </div>
+
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/recipes" component={Recipes} />
             <Route exact path="/recipes/:recipesId" component={SingleRecipes} />
             <Route path="/reviews/:recipesId" component={Reviews} />
-            <Route exact path="/shoppingList" component={ShoppingList} />
+            <Route exact path="/shoppinglist" component={ShoppingList} />
             {/* <Route
               exact
               path="/shoppingList/:shoppingListId"
